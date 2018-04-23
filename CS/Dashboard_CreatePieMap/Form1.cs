8faa4b5326afd3f7afad0f8c458ad6ee48c260c0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_CreatePieMap/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using DevExpress.DashboardCommon;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+using DevExpress.XtraEditors;
+
+namespace Dashboard_CreatePieMap {
+    public partial class Form1 : XtraForm {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Creates a new dashboard and data source for this dashboard.
+            Dashboard dashboard = new Dashboard();
+
+            DashboardSqlDataSource xmlDataSource = new DashboardSqlDataSource();
+            xmlDataSource.ConnectionParameters =
+                new XmlFileConnectionParameters(@"..\..\Data\DashboardEnergyStatictics.xml");
+            SelectQuery sqlQuery = SelectQueryFluentBuilder
+                .AddTable("Countries")
+                .SelectColumns("Latitude", "Longitude", "Production", "EnergyType", "Country")
+                .Build("Query 1");
+            xmlDataSource.Queries.Add(sqlQuery);
+            dashboard.DataSources.Add(xmlDataSource);
+
+            // Creates a Pie Map dashboard item and specifies its data source.
+            PieMapDashboardItem pieMap = new PieMapDashboardItem();
+            pieMap.DataSource = xmlDataSource;
+            pieMap.DataMember = "Query 1";
+
+            // Loads the map of the europe.
+            pieMap.Area = ShapefileArea.Europe;
+
+            // Provides countries coordinates.
+            pieMap.Latitude = new Dimension("Latitude");
+            pieMap.Longitude = new Dimension("Longitude");
+
+            // Specifies pie values and argument.
+            pieMap.Values.Add(new Measure("Production"));
+            pieMap.Argument = new Dimension("EnergyType");
+
+            // Specifies values displayed within pie tooltips.
+            pieMap.TooltipDimensions.Add(new Dimension("Country"));
+            pieMap.Legend.Visible = true;
+
+            // Adds the Pie Map dashboard item to the dashboard and opens this
+            // dashboard in the Dashboard Viewer.
+            dashboard.Items.Add(pieMap);
+            dashboardViewer1.Dashboard = dashboard;
+        }
+    }
+}
---
+++ VB/Dashboard_CreatePieMap/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.Sql
+Imports DevExpress.XtraEditors
+
+Namespace Dashboard_CreatePieMap
+    Partial Public Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            ' Creates a new dashboard and data source for this dashboard.
+            Dim dashboard As New Dashboard()
+
+            Dim xmlDataSource As New DashboardSqlDataSource()
+            xmlDataSource.ConnectionParameters =
+                New XmlFileConnectionParameters("..\..\Data\DashboardEnergyStatictics.xml")
+            Dim sqlQuery As SelectQuery = SelectQueryFluentBuilder.AddTable("Countries"). _
+                SelectColumns("Latitude", "Longitude", "Production", "EnergyType", "Country").Build("Query 1")
+            xmlDataSource.Queries.Add(sqlQuery)
+            dashboard.DataSources.Add(xmlDataSource)
+
+            ' Creates a Pie Map dashboard item and specifies its data source.
+            Dim pieMap As New PieMapDashboardItem()
+            pieMap.DataSource = xmlDataSource
+            pieMap.DataMember = "Query 1"
+
+            ' Loads the map of the europe.
+            pieMap.Area = ShapefileArea.Europe
+
+            ' Provides countries coordinates.
+            pieMap.Latitude = New Dimension("Latitude")
+            pieMap.Longitude = New Dimension("Longitude")
+
+            ' Specifies pie values and argument.
+            pieMap.Values.Add(New Measure("Production"))
+            pieMap.Argument = New Dimension("EnergyType")
+
+            ' Specifies values displayed within pie tooltips.
+            pieMap.TooltipDimensions.Add(New Dimension("Country"))
+            pieMap.Legend.Visible = True
+
+            ' Adds the Pie Map dashboard item to the dashboard and opens this
+            ' dashboard in the Dashboard Viewer.
+            dashboard.Items.Add(pieMap)
+            dashboardViewer1.Dashboard = dashboard
+        End Sub
+    End Class
+End Namespace
