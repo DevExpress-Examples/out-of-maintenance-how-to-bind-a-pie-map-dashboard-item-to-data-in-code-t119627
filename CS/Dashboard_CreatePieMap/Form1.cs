75266c448d464cb83c485656374c7d5be0d28a62
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_CreatePieMap/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using DevExpress.DashboardCommon;
+using DevExpress.XtraEditors;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+
+namespace Dashboard_CreatePieMap {
+    public partial class Form1 : XtraForm {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Creates a new dashboard and data source for this dashboard.
+            Dashboard dashboard = new Dashboard();
+
+            DashboardSqlDataSource dataSource = new DashboardSqlDataSource();
+            dataSource.ConnectionParameters =
+                new XmlFileConnectionParameters(@"..\..\Data\sochi2014.xml");
+            TableQuery sqlQuery = new TableQuery("Medals");
+            sqlQuery.AddTable("Table1").
+                SelectColumns("CapitalLat", "CapitalLon", "Quantity", "MedalClass", "Name");
+            dataSource.Queries.Add(sqlQuery);
+            dashboard.DataSources.Add(dataSource);
+
+            // Creates a Pie Map dashboard item and specifies its data source.
+            PieMapDashboardItem pieMap = new PieMapDashboardItem();
+            pieMap.DataSource = dataSource;
+            pieMap.DataMember = "Medals";
+
+            // Loads the map of the world.
+            pieMap.Area = ShapefileArea.WorldCountries;
+
+            // Provides cities' coordinates.
+            pieMap.Latitude = new Dimension("CapitalLat");
+            pieMap.Longitude = new Dimension("CapitalLon");
+
+            // Specifies pie values and argument.
+            pieMap.Values.Add(new Measure("Quantity"));
+            pieMap.Argument = new Dimension("MedalClass");
+
+            // Specifies values displayed within pie tooltips.
+            pieMap.TooltipDimensions.Add(new Dimension("Name"));
+
+            // Adds the Pie Map dashboard item to the dashboard and opens this
+            // dashboard in the Dashboard Viewer.
+            dashboard.Items.Add(pieMap);
+            dashboardViewer1.Dashboard = dashboard;
+        }
+    }
+}
---
+++ VB/Dashboard_CreatePieMap/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports System
+Imports DevExpress.DashboardCommon
+Imports DevExpress.XtraEditors
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.Sql
+
+Namespace Dashboard_CreatePieMap
+    Partial Public Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            ' Creates a new dashboard and data source for this dashboard.
+            Dim dashboard As New Dashboard()
+
+            Dim dataSource As New DashboardSqlDataSource()
+            dataSource.ConnectionParameters = New XmlFileConnectionParameters("..\..\Data\sochi2014.xml")
+            Dim sqlQuery As New TableQuery("Medals")
+            sqlQuery.AddTable("Table1").SelectColumns("CapitalLat", "CapitalLon", _
+                                                      "Quantity", "MedalClass", "Name")
+            dataSource.Queries.Add(sqlQuery)
+            dashboard.DataSources.Add(dataSource)
+
+            ' Creates a Pie Map dashboard item and specifies its data source.
+            Dim pieMap As New PieMapDashboardItem()
+            pieMap.DataSource = dataSource
+            pieMap.DataMember = "Medals"
+
+            ' Loads the map of the world.
+            pieMap.Area = ShapefileArea.WorldCountries
+
+            ' Provides cities' coordinates.
+            pieMap.Latitude = New Dimension("CapitalLat")
+            pieMap.Longitude = New Dimension("CapitalLon")
+
+            ' Specifies pie values and argument.
+            pieMap.Values.Add(New Measure("Quantity"))
+            pieMap.Argument = New Dimension("MedalClass")
+
+            ' Specifies values displayed within pie tooltips.
+            pieMap.TooltipDimensions.Add(New Dimension("Name"))
+
+            ' Adds the Pie Map dashboard item to the dashboard and opens this
+            ' dashboard in the Dashboard Viewer.
+            dashboard.Items.Add(pieMap)
+            dashboardViewer1.Dashboard = dashboard
+        End Sub
+    End Class
+End Namespace
